1cb7d26d078e6d3abddb72b07fc63ad8a9426867
C.J. Wade <95940433+sudge64@users.noreply.github.com>
2023-06-30 18:59:36 -0500

    Force `cp` by non-root user and update if statements to check for directories and files properly.

--- dotfiles.sh
+++ dotfiles.sh
@@ -15,51 +15,51 @@ done
 
 echo "Non sudo user name : $user_name"
 
-if [ ! $(ls /home/$user_name/.local/share/fonts) ]
+if [ ! -d /home/$user_name/.local/share/fonts ]
 then
     echo "Make /home/$user_name/.local/share/fonts"
-    mkdir /home/$user_name/.local/share/fonts
+    sudo -u $user_name mkdir /home/$user_name/.local/share/fonts
 fi
 
-if [ ! $(ls /home/$user_name/.local/share/fonts/Meslo*) ]
+if [ ! -f /home/$user_name/.local/share/fonts/Meslo* ]
 then
     echo "Download Meslo Nerd Font"
-    wget -P /home/$user_name/.local/share/fonts/ "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip"
+    sudo -u $user_name wget -P /home/$user_name/.local/share/fonts/ "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip"
     echo "Unzip Meslo Nerd Font"
-    unzip /home/$user_name/.local/share/fonts/Meslo.zip -d /home/$user_name/.local/share/fonts/
-    rm /home/$user_name/.local/share/fonts/Meslo.zip
+    sudo -u $user_name unzip /home/$user_name/.local/share/fonts/Meslo.zip -d /home/$user_name/.local/share/fonts/
+    sudo -u $user_name rm /home/$user_name/.local/share/fonts/Meslo.zip
 fi
 
-if [ ! $(ls /home/$user_name/.config/nvim) ]
+if [ ! -d /home/$user_name/.config/nvim ]
 then
     echo "Copy init.lua."
-    mkdir /home/$user_name/.config/nvim
-    cp ./files/init.lua /home/$user_name/.config/nvim/
+    sudo -u $user_name mkdir /home/$user_name/.config/nvim
+    sudo -u $user_name cp ./files/init.lua /home/$user_name/.config/nvim/
 fi
 
-if [ ! $(ls /home/$user_name/.config/kitty) ]
+if [ ! -d /home/$user_name/.config/kitty ]
 then
     echo "Copy kitty.conf."
-    mkdir /home/$user_name/.config/kitty
-    cp ./files/kitty.conf /home/$user_name/.config/kitty/
+    sudo -u $user_name mkdir /home/$user_name/.config/kitty
+    sudo -u $user_name cp ./files/kitty.conf /home/$user_name/.config/kitty/
 fi
 
-if [ ! $(ls /home/$user_name/.config/MangoHud) ]
+if [ ! -d /home/$user_name/.config/MangoHud ]
 then
     echo "Copy MangoHud.conf."
-    mkdir /home/$user_name/.config/MangoHud
-    cp ./files/MangoHud.conf /home/$user_name/.config/MangoHud/
+    sudo -u $user_name mkdir /home/$user_name/.config/MangoHud
+    sudo -u $user_name cp ./files/MangoHud.conf /home/$user_name/.config/MangoHud/
 fi
 
-if [ ! $(ls /home/$user_name/.p10k.zsh) ]
+if [ ! -f /home/$user_name/.p10k.zsh ]
 then
     echo "Copy .p10k.zsh."
-    cp ./files/.p10k.zsh /home/$user_name/
+    sudo -u $user_name cp ./files/.p10k.zsh /home/$user_name/
 fi
 
-if [ ! $(ls /home/$user_name/.zshrc) ]
+if [ ! -f /home/$user_name/.zshrc ]
 then
     echo "Copy .zshrc."
-    cp ./files/.zshrc /home/$user_name/
+    sudo -u $user_name cp ./files/.zshrc /home/$user_name/
 fi
 
