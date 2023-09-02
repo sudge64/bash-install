c0a21f1fbb0762231ce6d405225e94b79f3ef0ac
C.J. Wade <95940433+sudge64@users.noreply.github.com>
2023-09-01 23:27:22 -0500

    Modify `arch.sh` so that graphical programs are not installed when the user picks the server option.

--- arch.sh
+++ arch.sh
@@ -5,11 +5,31 @@
 # https://cj-wade.com
 # https://github.com/sudge64
 
-echo "Install cli packages."
-pacman -S $(cat packages/pacman_cli.txt) -y
+while getopts ":c:" option; do
+    case $option in
+        c) choice=$OPTARG;;
+        *) echo "Invalid Option"
+            exit;;
+    esac
+done
 
-echo "Install graphical packages."
-pacman -S $(cat packages/pacman_graphical.txt) -y
+function server_install(){
+    echo "Install cli packages."
+    pacman -S $(cat packages/pacman_cli.txt) -y
 
-echo "Install dependencies for Neovim."
-pacman -S base-devel cmake unzip ninja curl -y
+    echo "Install dependencies for Neovim."
+    pacman -S base-devel cmake unzip ninja curl -y       echo "Install cli packages."
+}
+
+if [ $choice -eq 1 ]
+then
+    server_install
+elif [ $choice -eq 2 ]
+then 
+    server_install
+    echo "Install graphical packages."
+    apt install $(cat packages/apt_graphical.txt) -y
+
+else
+    echo "invalid choice?"
+fi
