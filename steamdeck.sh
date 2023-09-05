59ff42af016f8b942b1a4a21d78229b435c55e17
C.J. Wade <95940433+sudge64@users.noreply.github.com>
2023-09-05 10:14:43 -0500

    Ad "CryoUtilities"

--- steamdeck.sh
+++ steamdeck.sh
@@ -5,6 +5,13 @@
 # https://cj-wade.com
 # https://github.com/sudge64
 
+echo "Emudeck"
 curl -v https://www.emudeck.com/EmuDeck.desktop -o EmuDeck.desktop
+
+echo "Decky Loader"
 curl -L https://github.com/SteamDeckHomebrew/deck-installer/releases/latest/download/install_release.sh | sh
 mv EmuDeck.desktop ~/Desktop
+
+echo "CryoUtilities"
+curl -v https://raw.githubusercontent.com/CryoByte33/steam-deck-utilities/main/InstallCryoUtilities.desktop -o InstallCryoUtilities.desktop
+mv InstallCryoUtilities.desktop ~/Desktop
