7e5e7d978590d1778fb3a7e77a24d604dbd37229
C.J. Wade <95940433+sudge64@users.noreply.github.com>
2023-09-14 12:31:25 -0500

    Improve verboseness and add `Set up tmux`

--- dotfiles.sh
+++ dotfiles.sh
@@ -17,22 +17,30 @@ echo "Non sudo user name : $user_name"
 
 if [ ! -f /home/$user_name/.config/nvim/init.lua ]
 then
-    echo "Copy init.lua."
+    echo "Set up neovim"
     sudo -u $user_name mkdir /home/$user_name/.config/nvim
     sudo -u $user_name cp ./files/init.lua /home/$user_name/.config/nvim/
     sudo -u $user_name cp ./files/lazy-lock.json /home/$user_name/.config/nvim/
 fi
 
+if [ ! -f /home/$user_name/.config/tmux/tmux.conf ]
+then
+    echo "Set up tmux"
+    sudo -u $user_name mkdir /home/$user_name/.config/tmux
+    sudo -u $user_name cp ./files/tmux.conf /home/$user_name/.config/tmux/
+    git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+fi
+
 if [ ! -f /home/$user_name/.config/kitty/kitty.conf ]
 then
-    echo "Copy kitty.conf."
+    echo "Set up kitty"
     sudo -u $user_name mkdir /home/$user_name/.config/kitty
     sudo -u $user_name cp ./files/kitty.conf /home/$user_name/.config/kitty/
 fi
 
 if [ ! -f /home/$user_name/.config/MangoHud/MangoHud.conf ]
 then
-    echo "Copy MangoHud.conf."
+    echo "Set up MangoHud"
     sudo -u $user_name mkdir /home/$user_name/.config/MangoHud
     sudo -u $user_name cp ./files/MangoHud.conf /home/$user_name/.config/MangoHud/
 fi
