061870d8913388f24f4b87774e13993343126cd2
C.J. Wade <95940433+sudge64@users.noreply.github.com>
2023-10-10 12:48:30 -0500

    Update

--- files/init.lua
+++ files/init.lua
@@ -129,16 +129,7 @@ require('lazy').setup({
       },
     },
   },
-
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
-  },
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
@@ -177,6 +168,7 @@ require('lazy').setup({
       vim.fn["mkdp#util#install"]()
     end,
   },
+  { 'christoomey/vim-tmux-navigator', lazy=false },
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
