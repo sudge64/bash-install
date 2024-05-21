a0d20035b555475d1687fd5cd25c914e6f815749
C.J. Wade <sudge64@icloud.com>
2024-05-20 21:11:02 -0500

    Add ""

--- arch.sh
+++ arch.sh
@@ -17,7 +17,7 @@ done
 
 function server_install(){
     echo "Install cli packages."
-    pacman -S $(cat packages/pacman_cli.txt) --noconfirm
+    pacman -S "$(cat packages/pacman_cli.txt)" --noconfirm
 
     echo "Install dependencies for Neovim."
     pacman -S base-devel cmake unzip ninja curl --noconfirm
@@ -27,54 +27,54 @@ function server_install(){
     echo 'unix_sock_group = "libvirt"' >> /etc/libvirt/libvirtd.conf
     echo 'unix_sock_rw_perms = "0770"' >> /etc/libvirt/libvirtd.conf
 
-    usermod -a -G libvirt $user_name
+    usermod -a -G libvirt "$user_name"
     newgrp libvirt
 
     echo "Remember! sudo systemctl start libvirtd.service"
 }
 
-if [ $choice -eq 1 ]
+if [ "$choice" -eq 1 ]
 then
     server_install
-elif [ $choice -eq 2 ]
+elif [ "$choice" -eq 2 ]
 then 
     server_install
     echo "Install graphical packages."
-    pacman -S $(cat packages/pacman_graphical.txt) --noconfirm
+    pacman -S "$(cat packages/pacman_graphical.txt)" --noconfirm
 else
     echo "invalid choice?"
 fi
 
-if [ $choice_hyprland -eq 1 ]
+if [ "$choice_hyprland" -eq 1 ]
 then
     echo "Installing Hyprland"
-    pacman -S $(cat packages/hyprland.txt) --noconfirm
+    pacman -S "$(cat packages/hyprland.txt)" --noconfirm
     gsettings set org.gnome.desktop.interface color-scheme prefer-dark
     echo "Installing Paru"
     git clone https://aur.archlinux.org/paru.git ../paru
     cd ../paru
     makepkg -si
     cd ../bash-install
-    paru -S $(cat packages/paru.txt)
+    paru -S "$(cat packages/paru.txt)"
 else
     echo "Not Installing Hyprland"
 fi
-pacman -S $(cat packages/pacman_framework.txt) --noconfirm
+pacman -S "$(cat packages/pacman_framework.txt)" --noconfirm
 
 if [ ! -f /etc/modprobe.d/hid_apple.conf ]
 then
     echo "Move hid_apple.conf"
-    sudo -u $user_name cp ./files/hid_apple.conf /etc/modprobe.d/
+    sudo -u "$user_name" cp ./files/hid_apple.conf /etc/modprobe.d/
 fi
 
 if [ ! -f /etc/modprobe.d/vfio.conf ]
 then
     echo "Move vfio.conf"
-    sudo -u $user_name cp ./files/vfio.conf /etc/modprobe.d/
+    sudo -u "$user_name" cp ./files/vfio.conf /etc/modprobe.d/
 fi
 
 if [ ! -f /etc/modules-load.d/v4l2loopback.conf ]
 then
     echo "Move v4l2loopback.conf"
-    sudo -u $user_name cp ./files/v4l2loopback.conf /etc/modules-load.d/
+    sudo -u "$user_name" cp ./files/v4l2loopback.conf /etc/modules-load.d/
 fi
