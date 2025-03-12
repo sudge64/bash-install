3da1e4150d07c95d131d8512dbd4c718527f6937
C.J. Wade <sudge64@icloud.com>
2025-03-11 21:38:45 -0500

    Streamline

--- steamdeck.sh
+++ steamdeck.sh
@@ -13,9 +13,5 @@ echo "Decky Loader"
 curl -v https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/decky_installer.desktop -o decky_installer.desktop
 mv decky_installer.desktop ~/Desktop
 
-echo "CryoUtilities"
-curl -v https://raw.githubusercontent.com/CryoByte33/steam-deck-utilities/main/InstallCryoUtilities.desktop -o InstallCryoUtilities.desktop
-mv InstallCryoUtilities.desktop ~/Desktop
-
 echo "Install Flatpaks"
 flatpak install "$(cat packages/flatpak_steamdeck.txt)" -y
