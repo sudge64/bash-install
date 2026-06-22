d7fb707ce37827503732d31a7bc64893cd661908
C.J. Wade <sudge64@icloud.com>
2026-06-22 09:03:26 -0500

    Update: `configs` -> `config`

--- files/nvim/lua/plugins/nvim-treesitter.lua
+++ files/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     dependencies = {'nvim-treesitter/nvim-treesitter-textobjects'},
     config = function()
-        local config = require("nvim-treesitter.configs")
+        local config = require("nvim-treesitter.config")
         config.setup({
             auto_install = true,
             highlight = { enable = true },
