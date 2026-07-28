ae2f6a62342cfaeec6864dc5a2f797a113ae8ab5
C.J. Wade <sudge64@icloud.com>
2026-07-27 21:45:19 -0500

    Update: Change Hyprland VRR status to 3.

--- files/hypr/hyprland.lua
+++ files/hypr/hyprland.lua
@@ -78,7 +78,7 @@ hl.config({
     misc = {
         force_default_wallpaper = 0,    -- Set to 0 or 1 to disable the anime mascot wallpapers
         disable_hyprland_logo   = true, -- If true disables the random hyprland logo / anime girl background. :(
-        vrr = 1,
+        vrr = 3
     },
     -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/XWayland/ for more
     -- So that xwayland programs are not pixelated.
